a0704af45ef4c63e53dc71f01de5f18500fc6221
Richie023 <85466516+Richie023@users.noreply.github.com>
2024-07-16 20:59:45 -0600

    Modelo y vista creadas ,falta el controller

---
+++ ArcadeX/Entidades/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArcadeX.Entidades
+{
+    public class Cliente
+    {
+        public String Identificacion { get; set; }
+        public String Nombre { get; set; }
+        public String Email { get; set; }
+        public String Contrasenna { get; set; }
+        public int RolID { get; set; }
+
+
+    }
+}
