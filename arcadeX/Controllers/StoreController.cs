d5db99979e0ff0f5218dffad46c1ea4386947da6
David Gómez <dagove47@gmail.com>
2024-08-14 02:30:10 -0600

    API Integration and Cards list display

--- arcadeX/Controllers/StoreController.cs
+++ arcadeX/Controllers/StoreController.cs
@@ -1,22 +1,42 @@
 ﻿using arcadeX.baseDatos;
 using arcadeX.Entidades;
 using arcadeX.Models;
-using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 
 namespace arcadeX.Controllers
 {
-
     public class StoreController : Controller
     {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
         UsuarioModel usuarioM = new UsuarioModel(); // Instancia correcta
+
         [HttpGet]
-        public ActionResult HomePage()
+        public async Task<ActionResult> HomePage()
         {
-            return View();
+            // Fetch the game data from the external API
+            var games = await GetGamesAsync();
+
+            // Pass the games data to the view
+            return View(games);
+        }
+
+        private async Task<List<Game>> GetGamesAsync()
+        {
+            var response = await _httpClient.GetAsync("https://www.freetogame.com/api/games");
+            response.EnsureSuccessStatusCode();
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+            var games = JsonConvert.DeserializeObject<List<Game>>(jsonString);
+
+            return games;
         }
 
         [HttpGet]
@@ -24,7 +44,6 @@ namespace arcadeX.Controllers
         {
             return View();
         }
-
 
         [HttpGet]
         public ActionResult RegistroUsuario()
--- arcadeX/Models/StoreModel.cs
+++ arcadeX/Models/StoreModel.cs
@@ -5,7 +5,13 @@ using System.Web;
 
 namespace arcadeX.Models
 {
-    public class StoreModel
+    public class Game
     {
+        public string Title { get; set; }
+        public string Thumbnail { get; set; }
+        public string Genre { get; set; }
+        public string Platform { get; set; }
+        public string Publisher { get; set; }
+        public string Release_Date { get; set; }
     }
 }
